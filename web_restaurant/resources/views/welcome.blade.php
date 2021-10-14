09d7b617b630012054a665ec81fa87d222ff3fdc
Yong Chun Yee <p19011257@student.newinti.edu.my>
2021-10-14 16:32:33 +0800

    modify own branch homepage

--- web_restaurant/resources/views/welcome.blade.php
+++ web_restaurant/resources/views/welcome.blade.php
@@ -1,7 +1,6 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
-<<<<<<< HEAD
         <title>Laravel</title>
 
         <!-- Fonts -->
@@ -17,58 +16,7 @@
                 font-family: 'Nunito', sans-serif;
             }
         </style>
-    </head>
-    <body class="antialiased">
-        <div class="relative flex items-top justify-center bg-gray-100 focus:bg-black sm:items-center py-4 sm:pt-0">
-            @if (Route::has('login'))
-                <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
-                    @auth
-                        <a href="{{ url('/addRes') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Add Restaurant</a>
-                        <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Dashboard</a>
-                    @else
-                    <a href="{{ url('/addRes') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Add Restaurant</a>
-                        <a href="{{ route('login') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-        </div>
-
-            <div class="container">
-                        <link href="assets/css/material-kit.css" rel="stylesheet" />
-                    <div class="py-12">
-                    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                        <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                        <div class="page-header header-filter" data-parallax="true" style="background-image: url('assets/img/bg3.jpg')">
-                <div class="container">
-                <div class="row">
-                    <div class="col-md-8 ml-auto mr-auto">
-                    <div class="brand text-center">
-                        <h1>Your title here</h1>
-                        <h3 class="title text-center">Your Subtitle here</h3>
-                    </div>
-                    </div>
-                </div>
-                </div>
-            </div>
-            <div class="main main-raised">
-                <div class="container">
-                <div class="section text-center">
-                    <h2 class="title">Your main section here</h2>
-                </div>
-                </div>
-            </div>
-                </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        
-    </body>
-=======
 <head>
     <title>Hello, Restaurant!</title>
     <!-- Required meta tags -->
@@ -159,5 +107,4 @@
 </body>
 @include('footer')
 
->>>>>>> 8fe840ff9decdd7367ab18332db5a0b096e10e0b
 </html>
