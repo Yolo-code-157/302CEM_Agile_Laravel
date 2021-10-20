110bd37adfceec54f576575e6f76051f953584c4
yukilau15 <yukilau15@gmail.com>
2021-10-20 13:09:53 +0800

    Detail and Review UI

--- web_restaurant/resources/views/dashboard.blade.php
+++ web_restaurant/resources/views/dashboard.blade.php
@@ -11,13 +11,19 @@
         <!-- Fonts and icons -->
         <link rel="stylesheet" type="text/css"
             href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
-        <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" rel="stylesheet">    
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" rel="stylesheet">
         <!--link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css"-->
 
         <!-- Material Kit CSS -->
         <link href="/assets/css/addRes.css" rel="stylesheet" />
 
         <style>
+            .img-fluid {
+                object-fit: cover;
+                width: 100%;
+                height: 171px;
+            }
+
             .border-radius-lg {
                 border-radius: .5rem;
             }
@@ -42,61 +48,87 @@
             <div class="container">
                 <div class="pt-5">
                     <form name="" method="get" class="flex">
-                        <input type="search" name="search" placeholder="Search" class="form-control m-xl-3" required>
-                        <input type="submit" name="" class="btn btn-primary btn-round m-xl-3">
+                        <input type="search" name="search" placeholder="Search" class="form-control m-xl-3">
+                        <input type="submit" name="" class="btn btn-primary m-xl-3">
                     </form>
                 </div>
-                <div class="">
-                    @if ($query != "") 
-                    {{-- the query is no empty --}}
-                        <div class="search-container">
-                            {{-- Remove HTML tag and get the text --}}
-                            <h5 class="">{!!$query!!}</h5>
+                <div>
+                    @if ($query != null)
+                        <h5 class="text-center h2">{!! $query !!}</h5>
+                        {{-- the query is no empty --}}
+                        <div class="row section">
                             {{-- if the search restaurant found --}}
                             @if (count($searchRes) > 0)
-                                <div class="pt-5 row search-row">
-                                    @foreach($searchRes as $search)  
+                                @foreach ($searchRes as $search)
                                     <!--Retrieving all the data based on the search query-->
                                     <div class="col-lg-3 col-sm-6 mb-sm-3">
                                         <div class="card-plain">
                                             <div class="card-header p-0">
-                                                <img src="data:image/{{base64_encode($search->resPicType)}};charset=utf8;base64,{{base64_encode($search->resPic)}}" 
-                                                class="img-fluid border-radius-lg" alt="restaurant image" loading="lazy"/>
+                                                <img src="data:image/{{ base64_encode($search->resPicType) }};charset=utf8;base64,{{ base64_encode($search->resPic) }}"
+                                                    class="img-fluid border-radius-lg" alt="restaurant image"
+                                                    loading="lazy" />
                                             </div>
                                             <div class="card-body px-0">
-                                                <h3>
-                                                <a href="#" class="text-dark font-weight-bold">{{ $search -> resName }}</a>
-                                                </h3>
-                                                <h5>Postcode: {{ $search -> resPostcode }}</h5>
-                                                <a href="viewRes/{{$search->resID}}" class="text-primary text-sm">Read More</a>
+                                                <h4>
+                                                    <a href="viewRes/{{ $search->resID }}"
+                                                        class="text-dark font-weight-bold">{{ $search->resName }}</a>
+                                                </h4>
+                                                <h6>Postcode: {{ $search->resPostcode }}</h6>
+                                                <a href="viewRes/{{ $search->resID }}"
+                                                    class="text-primary text-sm">Read More</a>
                                             </div>
                                         </div>
-                                    </div>                
-                                    @endforeach
-                                </div>
+                                    </div>
+                                @endforeach
+                            @else
+                                @foreach ($users as $row)
+                                    <!--Retrieving all the data-->
+                                    <div class="col-lg-3 col-sm-6 mb-sm-3">
+                                        <div class="card-plain">
+                                            <div class="card-header p-0">
+                                                <img src="data:image/{{ base64_encode($row->resPicType) }};charset=utf8;base64,{{ base64_encode($row->resPic) }}"
+                                                    class="img-fluid border-radius-lg" alt="restaurant image"
+                                                    loading="lazy" />
+                                            </div>
+                                            <div class="card-body px-0">
+                                                <h4>
+                                                    <a href="viewRes/{{ $row->resID }}"
+                                                        class="text-dark font-weight-bold">{{ $row->resName }}</a>
+                                                </h4>
+                                                <h6>Postcode: {{ $row->resPostcode }}</h6>
+                                                <a href="viewRes/{{ $row->resID }}" class="text-primary text-sm">Read
+                                                    More</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                @endforeach
                             @endif
-                        </div>                
+                        </div>
+                    @else
+                        <div class="row section">
+                            @foreach ($users as $row)
+                                <!--Retrieving all the data-->
+                                <div class="col-lg-3 col-sm-6 mb-sm-3">
+                                    <div class="card-plain">
+                                        <div class="card-header p-0">
+                                            <img src="data:image/{{ base64_encode($row->resPicType) }};charset=utf8;base64,{{ base64_encode($row->resPic) }}"
+                                                class="img-fluid border-radius-lg" alt="restaurant image"
+                                                loading="lazy" />
+                                        </div>
+                                        <div class="card-body px-0">
+                                            <h4>
+                                                <a href="viewRes/{{ $row->resID }}"
+                                                    class="text-dark font-weight-bold">{{ $row->resName }}</a>
+                                            </h4>
+                                            <h6>Postcode: {{ $row->resPostcode }}</h6>
+                                            <a href="viewRes/{{ $row->resID }}" class="text-primary text-sm">Read
+                                                More</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            @endforeach
+                        </div>
                     @endif
-                    <div class="row section">
-                        @foreach($users as $row)  
-                        <!--Retrieving all the data-->
-                        <div class="col-lg-3 col-sm-6 mb-sm-3">
-                            <div class="card-plain">
-                                <div class="card-header p-0">
-                                    <img src="data:image/{{base64_encode($row->resPicType)}};charset=utf8;base64,{{base64_encode($row->resPic)}}" 
-                                    class="img-fluid border-radius-lg" alt="restaurant image" loading="lazy"/>
-                                </div>
-                                <div class="card-body px-0">
-                                    <h3>
-                                        <a href="#" class="text-dark font-weight-bold">{{ $row -> resName }}</a>
-                                    </h3>
-                                    <h5>Postcode: {{ $row -> resPostcode }}</h5>
-                                    <a href="viewRes/{{$row->resID}}" class="text-primary text-sm">Read More</a>
-                                </div>
-                            </div>
-                        </div>                
-                        @endforeach
-                    </div>
                 </div>
             </div>
         </div>
--- web_restaurant/resources/views/resRating.blade.php
+++ web_restaurant/resources/views/resRating.blade.php
@@ -1,132 +1,129 @@
 <x-app-layout>
-    <!DOCTYPE html>
     <html lang="en">
+
     <head>
-    <title>Hello, Restaurant!</title>
-            <!-- Required meta tags -->
-            <meta charset="utf-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+        <title>Hello, Restaurant!</title>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 
-            <!-- Fonts and icons -->
-            <link rel="stylesheet" type="text/css"
-                href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
-            <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" rel="stylesheet">    
-            <!--link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css"-->
+        <!-- Fonts and icons -->
+        <link rel="stylesheet" type="text/css"
+            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" rel="stylesheet">
+        <!--link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css"-->
 
-            <!-- Material Kit CSS -->
-            <link href="/assets/css/resRating.css" rel="stylesheet" />
+        <!-- Material Kit CSS -->
+        <link href="/assets/css/resRating.css" rel="stylesheet" />
 
-            <style>
-                .border-radius-lg {
-                    border-radius: .5rem;
-                }
+        <style>
+            .img-fluid {
+                object-fit: cover;
+                width: 100%;
+                height: 430px;
+            }
 
-            </style>
+            .border-radius-lg {
+                border-radius: .5rem;
+            }
+
+        </style>
     </head>
+
     <body>
-        <div class="page-header header-filter" data-parallax="true" style="background-image: url('img/bg3.jpg')">
+        <div class="page-header header-filter" data-parallax="true" style="background-image: url(/img/bg7.jpg)">
             <div class="container">
                 <div class="row">
                     <div class="col-md-8 ml-auto mr-auto">
                         <div class="brand text-center">
-                            <h1>Add Comments & Ratings</h1>
+                            <h1>Write A Review</h1>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-       
-        <div class="form-group">
-        <div class="container">
-                <h4 style="text-align:center ; font-weight:bold;">Restaurant Name: {{$Detail -> resName}}</h4>
-                <h4 style="text-align:center ; font-weight:bold;">Postcode: {{$Detail -> resPostcode}}</h4>
-        </div>
-                <div class="form-group">
-                    <!-- Create the editor container -->
-                    <div id="content" name="Description" style="height:300px">
+
+        <div class="main main-raised">
+            <div class="container">
+                <div class="row section">
+                    <div class="col">
+                        <img src="data:image/{{ base64_encode($Detail->resPicType) }};charset=utf8;base64,{{ base64_encode($Detail->resPic) }}"
+                            class="img-fluid border-radius-lg" alt="restaurant image" loading="lazy" />
                     </div>
-                    <textarea class="form-control" id="content-textarea" type="textarea"
-                        style="display:none" rows="4" name="description"></textarea><br>
+                    <div class="col">
+                        <div class="card-header p-0">
+                            <h3 class="text-primary">{{ $Detail->resName }}</h3>
+                            <p>
+                                <i class="fas fa-map-marker-alt text-primary"></i>
+                                {{ $Detail->resPostcode }}
+                            </p>
+                        </div>
+                        <form role="form" method="post">
+                            <div class="card-body px-0">
+                                <div class="row">
+                                    <div class="col">
+                                        <label>Service: </label><br>
+                                        <input type="range" id="service_vol" name="service_vol" min="0" max="5" value="0">
+                                        <br>
+                                        <p>Value: <span id="service_no"></span></p>
+                                        <br>
+                                    </div>
+
+                                    <div class="col">
+                                        <label>Value: </label><br>
+                                        <input type="range" id="value_vol" name="value_vol" min="0" max="5" value="0">
+                                        <br>
+                                        <p>Value: <span id="value_no"></span></p>
+                                        <br>
+                                    </div>
+
+                                    <div class="col">
+                                        <label>Food: </label><br>
+                                        <input type="range" id="food_vol" name="food_vol" min="0" max="5" value="0">
+                                        <br>
+                                        <p>Value: <span id="food_no"></span></p>
+                                        <br>
+                                    </div>
+                                </div>
+
+                                <label>Your Review</label>
+                                <textarea name="review" class="form-control" rows="4" required></textarea>
+                                <input type="submit" class="btn bg-primary w-100 mt-3">
+                            </div>
+                        </form>
+                    </div>
                 </div>
+            </div>
         </div>
-        <div class="service">
-            <p>Service</p>
-            <input type="range" min="1" max="5" value="1" id="myRating" class="serviceslider">
-            <p>Value:<span id="value"></span></p>
-        </div>
-        <div class="ratings">
-            <p>Value</p>
-            <input type="range" min="1" max="5" value="1" id="myValue" class="slider">
-            <p>Value:<span id="value2"></span></p>
-        </div>
-        <div class="food">
-            <p>Food</p>
-            <input type="range" min="1" max="5" value="1" id="myFood" class="foodslider">
-            <p>Value:<span id="value3"></span></p>
-        </div>
-        <div class="section text-center">
-        <input type="submit" class="btn btn-primary btn-round" name="add_rat"><br>  
-        </div>     
-    </body>  
-    <script>
-        var serviceslider= document.getElementById("myRating")
-        var outputslider=document.getElementById("value")
-
-        outputslider.innerHTML=serviceslider.value;
-        serviceslider.oninput=function(){
-        outputslider.innerHTML=this.value;
-        }
-        serviceslider.addEventListener("mousemove",function(){
-            var testing= serviceslider.value;
-            var color='linear-gradient(90deg, rgb(117,252,117)' + testing + '%,rgb(214,214,214)'+ testing*20 +'%)';
-            serviceslider.style.background=color;
-        })
-    </script>
-    <script>
-        var slider= document.getElementById("myValue")
-        var output=document.getElementById("value2")
-
-        output.innerHTML=slider.value;
-        slider.oninput=function(){
-        output.innerHTML=this.value;
-        }
-        slider.addEventListener("mousemove",function(){
-            var testing= slider.value;
-            var color='linear-gradient(90deg, rgb(117,252,117)' + testing + '%,rgb(214,214,214)'+ testing*20 +'%)';
-            slider.style.background=color;
-        })
-    </script>
-    <script>
-        var foodslider= document.getElementById("myFood")
-        var outputfood=document.getElementById("value3")
-
-        outputfood.innerHTML=foodslider.value;
-        foodslider.oninput=function(){
-        outputfood.innerHTML=this.value;
-        }
-        foodslider.addEventListener("mousemove",function(){
-            var testing= foodslider.value;
-            var color='linear-gradient(90deg, rgb(117,252,117)' + testing + '%,rgb(214,214,214)'+ testing*20 +'%)';
-            foodslider.style.background=color;
-        })
-    </script>
+    </body>
     @include('footer')
-    </html>
-</x-app-layout>   
-<link href="https://cdn.quilljs.com/1.3.6/quill.snow.css" rel="stylesheet">
-    <script src="https://cdn.quilljs.com/1.3.6/quill.js"></script>
 
-    <!-- Initialize Quill editor -->
     <script>
-    $(document).ready(function() {
-        var quill = new Quill('#content', {
-            placeholder: 'Add your message in ...',
-            theme: 'snow'
-        });
+        var serviceVol = document.getElementById("service_vol")
+        var serviceNo = document.getElementById("service_no")
 
-        quill.on('text-change', function(delta, oldDelta, source) {
-            $('#content-textarea').text($(".ql-editor").html());
-        })
-    })
+        var valueVol = document.getElementById("value_vol")
+        var valueNo = document.getElementById("value_no")
+
+        var foodVol = document.getElementById("food_vol")
+        var foodNo = document.getElementById("food_no")
+
+        serviceNo.innerHTML = serviceVol.value;
+        serviceVol.oninput = function() {
+            serviceNo.innerHTML = this.value;
+        }
+
+        valueNo.innerHTML = valueVol.value;
+        valueVol.oninput = function() {
+            valueNo.innerHTML = this.value;
+        }
+
+        foodNo.innerHTML = foodVol.value;
+        foodVol.oninput = function() {
+            foodNo.innerHTML = this.value;
+        }
     </script>
+
+    </html>
+</x-app-layout>
